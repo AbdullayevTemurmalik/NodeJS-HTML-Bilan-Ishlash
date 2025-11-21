02dd7602dce57536a5fdf047713cb8d6805797f4
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-21 15:37:04 +0500

    HTML-bilan-Ishlash

---
+++ events.js
@@ -0,0 +1,17 @@
+const Events = require("events");
+
+class Logger extends Events {
+  log(a, b) {
+    this.emit("calculate", a + b);
+  }
+}
+const logger = new Logger();
+
+logger.on("calculate", (data) => {
+  console.log(data);
+});
+
+logger.log(8, 11);
+logger.log(2, 11);
+logger.log(5, 11);
+logger.log(15678, 2341);
