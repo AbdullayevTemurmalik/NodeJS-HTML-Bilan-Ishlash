95ca229de19dc85b73420ecd6ecfcf552feffaa5
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-21 15:44:21 +0500

    HTML-bilan-Ishlash

--- index.js
+++ index.js
@@ -10,31 +10,43 @@ const server = http.createServer((req, res) => {
       fs.readFile(
         path.join(__dirname, "templates", "index.html"),
         "utf-8",
-        (err, conten) => {
+        (err, content) => {
           if (err) throw err;
-          res.end(conten);
+          res.end(content);
         }
       );
     } else if (req.url === "/about") {
-        fs.readFile(
+      fs.readFile(
         path.join(__dirname, "templates", "about.html"),
         "utf-8",
-        (err, conten) => {
+        (err, content) => {
           if (err) throw err;
-          res.end(conten);
+          res.end(content);
         }
       );
+    } else if (req.url === "/contact") {
+      fs.readFile(
+        path.join(__dirname, "templates", "contact.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    }
   } else if (req.method === "POST") {
     const body = [];
 
     req.on("data", (data) => {
-      body.push(Buffer.from(data));
+      body.push(data);
     });
 
     req.on("end", () => {
-      const message = body.toString().split("=")[1];
+      const parsed = Buffer.concat(body).toString();
+      const message = parsed.split("=")[1];
 
-      res.end(`name successfully added: ${message}`);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`Name successfully added: ${message}`);
     });
   }
 });
